f35733e8b89d1d9e2d2107fe21d2678f2ac7f4a0
Edwin_Eddie <idongesitedidiong123@gmail.com>
2019-07-09 01:09:28 +0100

    Creating Models,Controllers and Migration tables

---
+++ gscc/database/factories/BlwFellowshipInvolvementDetailFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Model\BlwFellowshipInvolvementDetail;
+use Faker\Generator as Faker;
+
+$factory->define(BlwFellowshipInvolvementDetail::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ gscc/database/factories/EducationalQualificationFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Model\EducationalQualification;
+use Faker\Generator as Faker;
+
+$factory->define(EducationalQualification::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
